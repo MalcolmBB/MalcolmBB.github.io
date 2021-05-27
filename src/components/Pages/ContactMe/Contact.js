193ee42e3e10f9db5539b174c2452975e4dfea49
Malcolm <BTJMAL001@myuct.ac.za>
2021-05-28 00:51:40 +0200

    Added privacy policy and sitemap link. Sitemap needs to be made. Contact page needs to be made. About page needs to be made.

---
+++ src/components/Pages/ContactMe/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../../Header/Header'
+import Footer from '../../Footer/Footer'
+
+class Contact extends React.Component {
+    render(){
+        return (
+            <div>
+                <Header></Header>
+
+                
+
+
+                <Footer></Footer>
+            </div>
+        );
+    }
+}
+
+export default Contact;
