b970683ba2519ce9d607f469ae2827c8e83819f1
Malcolm Baatjies <malcolmbaatjies@gmail.com>
2022-04-14 23:26:17 +0200

    Fixed main summary
    Updated keys

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -100,22 +100,20 @@ function Home(props) {
 				>
 					<Header></Header>
 					<div className="mainContainer">
-						<AboutContent
-							type="Button"
-							sideKey="-1"
-							Title="About me"
-							imgSrc="../images/AboutMePicture.jpg"
-							imgAlt="Malcolm Baatjies"
-							paragraph={[
-								"My name is Malcolm Baatjies. I am currently in my third year at The University of Cape Town studying a Bachelors in Science majoring in Computer Science and Computer Engineering. I have had a passion for Computers for most of my life. Some of my earliest memories include me exploring our family shared computer. From browsing around the operating system to exploring and trying to understand Excel games. This curiousity has followed me to this day. I am constantly trying to learn to new things and understand how more complex systems work.",
-								"\nI have experimented with many different programming languages including Python, Java, C++, and JavaScript and I am eager to keep expanding this list. Currently my personal interest is in web design and specifically React. I plan on pursuing an Honours degree in 2022 however future personal projects include new websites and mobile applications.",
-								"\nI am a very results driven person and I pride myself on going above and beyond the expectations of the project. I work well in a team and can often take charge and delegate responsibilities but also know when to step back and follow someone elses lead. I respect authority very well, but will also speak my mind and present my own ideas if the moment calls for it. To see more about my education and aspirations click the button below to navigate to the about tab",
-							]}
-							buttonClassName="button bAboutContainer"
-							buttonLinkTo="/About"
-							buttonType="Navigation"
-							buttonValue="More about me!"
-						></AboutContent>
+						{MainSummary.map((ms, key) => (
+							<AboutContent
+								type="Button"
+								sideKey="-1"
+								Title="About me"
+								imgSrc="../images/AboutMePicture.jpg"
+								imgAlt="Malcolm Baatjies"
+								paragraph={ms.paragraph}
+								buttonClassName="button bAboutContainer"
+								buttonLinkTo="/About"
+								buttonType="Navigation"
+								buttonValue="More about me!"
+							></AboutContent>
+						))}
 						<div className="PastWorkContainer">
 							<h3>Some of my past work</h3>
 							{arr.map((wl, key) => (
